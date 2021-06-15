cb542e711b3dd693aaf29dab45c3dd672832fd81
ShaikAkbar000 <85789810+ShaikAkbar000@users.noreply.github.com>
2021-06-15 17:00:28 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+
+<head>
+  <meta charset="utf-8">
+  <title> Bank of Rivera </title>
+  <style>
+    body {
+      padding-top: 60px;
+      font-size:25px;
+      background: #40E0D0;
+      background: -webkit-linear-gradient(to right, #AFEEEE, #79f2e6 );
+      background: linear-gradient(to right, #AFEEEE, #79f2e6 );
+    }
+    .center{
+      padding-top:5px;
+      background: #FFFFF;
+      display: block;
+      margin-top: 20px;
+      margin-left: auto;
+      margin-right: auto;
+      width: 50%
+    }
+ 
+  </style>
+</head>
+
+<body>
+  <?php include('navbar.php'); ?>
+  
+  <div class="center">
+      <h1 style="text-align: center">Welcome to Bank of Rivera<h1>
+      <img class ="center" src="bankImage.png" alt="" width="150" height="300">
+      <p style="text-align: center ; font-size: 20px">Designed and Coded by: Akbar Ahmed<p>
+     
+  </div>
+    
+ 
+
+
+</body>
+
+</html>
